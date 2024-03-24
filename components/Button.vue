6885794d1ab285c9649c26bc0fd82e84e50f0dcf
Mehedi Hasan Ahad <mehedihasanahad07@gmail.com>
2024-03-25 02:57:02 +0600

    first commit

---
+++ components/Button.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <button>click</button>
+  </div>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <p>Some default layout content shared across all pages</p>
+    <slot />
+  </div>
+</template>
+
+
+<script setup>
+
+</script>
